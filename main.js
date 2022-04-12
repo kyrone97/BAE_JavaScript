8724348a334ec802094ba1c6277a9180316113ae
Kyrone James <kyrone@Kyrones-MacBook-Pro.local>
2022-04-12 10:43:49 +0100

    basic javascript intro

---
+++ main.js
@@ -0,0 +1,57 @@
+// javascript != java
+// how to print
+console.log("hello world")
+
+// Declaring Variables
+//let // variable that can change
+//const // variable that wont change
+
+// Number
+
+const age = 10;
+const salary = 10.50;
+
+// String
+
+let surname = "Smith"
+
+// array can have multiple data types
+
+let myArray = [10,12,13,"hello"];
+
+console.log(typeof age);
+console.log(typeof salary)
+
+console.log(myArray[0])
+
+// literal
+
+const literalstuff = ` my surname is ${surname}`
+console.log(literalstuff)
+
+console.log("Kyrone");
+
+console.info("James");
+
+console.warn("London");
+
+console.error("SAGGITARIUS");
+
+
+let myCar = "BMW";
+let myModel = "M3";
+console.log("my fav car is: " + myCar + " and the model is: " + myModel);
+console.log(`my favourite car is ${myCar} and the model is ${myModel}`)
+
+
+console.log("This is now %c the end of the exercises.","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
+
+    
+
+  
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay = totalMoney - moneyPaidSoFar;
+
+console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`);
+
