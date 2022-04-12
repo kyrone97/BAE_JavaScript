18664d74c71a43b9f880a6d93777087d2958b08d
Kyrone James <kyrone@Kyrones-MacBook-Pro.local>
2022-04-12 16:21:37 +0100

    Ternary example

--- iteration.js
+++ iteration.js
@@ -1,37 +1,4 @@
-function blackjack( a, b) {
-  if (a > 21 && b > 21) {
-      return 0;
-  }else if (a > 21) {
-      return b;
-  } else if (b > 21) {
-      return a;
-  }
-   
-   let sumA = 21 - a;
-   let sumB = 21 - b;
-   
-  if (sumA > sumB) {
-      return b;
-  } else {
-      return a;
-  }
-   }
 
-   console.log(blackjack(10,21,))
-   console.log(blackjack(20,18,))
-   console.log(blackjack(1,22,))
-   console.log(blackjack(22,23,))
+// Ternary Example
 
-
-//    Incorrect 
-   function uniqueSum (q, t, u) {
-       if (q === t || t === u || q === u)
-       sum === 0  
-       else sum = q + t + u
-       return sum
-      
-   }
-
-    console.log(uniqueSum(1,2,3,))
-    console.log(uniqueSum(3,3,3,))
-    console.log(uniqueSum(1,1,2,))
+(1 + 1 === 2) ? console.log("True") : console.log("False")
