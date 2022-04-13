d7678f0e80336ec03faa114b34d8c643c5a0b4d6
Kyrone James <kyrone@Kyrones-MacBook-Pro.local>
2022-04-13 11:28:30 +0100

    More advanced example of the arrays and object with advanced for loop

--- day2.js
+++ day2.js
@@ -40,17 +40,31 @@ console.log(kyCar["greeting"])
 
 //  same example above but you can have multiple arrays in the object
 
-let kyCars = {
+let dansCars = {
     car:{
-    name: "Super Car",
-    make: "Bmw",
-    model: "M5 cs",
-    year: "2019",
-    colour: "lilac",
+    name: "Sports Car",
+    make: "Mercedes",
+    model: "C63",
+    year: "2016",
+    colour: "Blue",
     "greeting":"hello"
     }
 }
 //  can add extra to end of the list 
-kyCars ["Engine"] = " V8 TURBO"
-console.log(kyCars.car)
-console.log(kyCars.car.name)
+dansCars ["Engine"] = " V8 TURBO"
+console.log(dansCars.car)
+console.log(dansCars.car.name)
+
+// make an array to store cars
+
+let garage = [ kyCar,dansCars, {"make":"Renault", "model":"Clio"}]
+console.log(garage)
+console.log("\n")  // new line
+
+// ENHANCED FOR LOOP
+// initialisation , condition, after effect
+//  shows all the makes of all the cars in the garage
+for (let car in garage) {
+    console.log(garage[car].make)
+}
+
